c747f51b19754d60e4cc49086fc7c501c7e8a733
Rosh <roshramesh97@gmail.com>
2025-04-21 19:49:28 -0600

    Added primary intro screen transition + signup screen - Colour to be fixed

---
+++ yes_app/lib/core/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+/// Centralized color palette.
+class AppColours {
+  static const Color primaryPurple = Color(0xFF7F00FF);
+  static const Color primaryBlue = Color(0xFF00B4DB);
+  static const Color backgroundWhite = Color(0xFFFFFFFF);
+
+// Define more custom colors here
+}
